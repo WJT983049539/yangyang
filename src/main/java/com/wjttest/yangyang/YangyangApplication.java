6ec0136413462e5eedda1ef6e75a881c33fe013b
983049539 <983049539@qq.com>
2020-11-16 11:43:26 +0800

    springboot 学习计划提交项目

---
+++ src/main/java/com/wjttest/yangyang/YangyangApplication.java
@@ -0,0 +1,13 @@
+package com.wjttest.yangyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangyangApplication {
+    //这是入口
+    public static void main(String[] args) {
+        SpringApplication.run(YangyangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wjttest/yangyang/YangyangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjttest.yangyang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YangyangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
